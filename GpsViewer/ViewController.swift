6406396b815a366fbfc058a00c544130d86efffe
ikeda-mk <2306915+ikeda-mk@users.noreply.github.com>
2019-09-02 00:15:25 +0900

    init commit

---
+++ GpsViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GpsViewer
+//
+//  Created by mak on 2019/09/02.
+//  Copyright © 2019 example.dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
